2dbd46e5a9427b35fd0d4314b2eeacde0172b5de
hodiJavaQa <99143515+hodiJavaQa@users.noreply.github.com>
2022-02-12 15:07:25 +0200

    Add files via upload

---
+++ hoditheking.java
@@ -0,0 +1,127 @@
+public class hoditheking {
+    public static void main(String[] args) {
+
+        //int age = 5;
+        //double salary=50000.0;
+        //System.out.println(salary);
+        //System.out.println(age);
+
+        //int x=12,y=10;
+        //int z;
+        //System.out.println("x is "+x);
+
+        //x++; //x=x+1
+        //x--;//x=x-1
+
+        //System.out.println("x is "+x);
+        //z=x+y;      // 22
+        //System.out.println("x is "+x);
+        //System.out.println("y is "+y);
+        //System.out.println("x+y is "+z);
+
+
+        // כתבו קוד שמכיל 3 מספרים שלמים ומחשב את הממוצע שלהם
+        //תדפיסו את הממוצע למסך
+        //int a =12;
+        //int b =100;
+        //int c= 15;
+        //int avg;
+        ////()  ^2   */      -+
+        //System.out.println(avg);
+        //avg=(a+b+c)/3;
+
+
+        //int x =10 ,y=13;
+        //System.out.println(x);
+        //System.out.println(15==3);
+//
+        //x=y;
+//
+        //System.out.println(x == y);
+        //System.out.println(x);
+        //int x =-3,y=-8;
+        //boolean isXgreaterThanY  = x  >  y   ;
+//
+        //System.out.println(isXgreaterThanY);
+//
+//
+//
+        //String name = "yuval";
+//
+        //if (name == "lian") {
+        //    System.out.println("hiii  " + name);
+        //    name = "hodi";
+        //}
+//
+        //System.out.println("bye bye");
+
+        // כתבו קוד בגאווה תגדירו משתנה אחד שלם
+        // תבדקו אם המספר חיובי תדפיסו אותו ותכתבו בי ביי בכל מקרה
+        //int number=1;
+//
+        //if (number >= 0) {
+        //    System.out.println("number is " + number);
+        //}
+//
+        //System.out.println("have a nice day");
+
+        double money = 23;
+        System.out.println("form haifa");
+
+        if (money >= 25) {
+            System.out.println("gvish  666 ");
+        } else {
+            System.out.println("gvish 222");
+        }
+
+        System.out.println("arrived to tel aviv baby");
+
+        // כתבו תוכנית בשפת גאווה
+        // הגדירו 2 משתנים ממשיים שונים זה מזה
+        // תבדקו אם המספר הראשון קטן  מחצי המספר בשני
+        //  d1 less than d2 /2 ---- d2   *0.5
+        // אם כן תדפיסו YES
+        // אחרת תדפיסו NO
+
+        double d1 = 58, d2 = 100;
+
+        if (d1 < d2 / 2) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
